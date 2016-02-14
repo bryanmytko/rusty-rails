f27d81e06b252aebe54d09179bd087a3ba73061d
Bryan Mytko <bryanmytko@gmail.com>
2016-02-14 01:17:28 -0500

    Use establish_connection

--- app/controllers/entries/mod.rs
+++ app/controllers/entries/mod.rs
@@ -11,26 +11,26 @@ mod views;
 use models::entry::Entry;
 use schema::entries::dsl::entries as Entries;
 use persistent::Read;
-// use helpers::db::establish_connection;
+use helpers::db::establish_connection;
 
 pub fn index(request: &mut Request) -> IronResult<Response> {
-    let connection = request.extensions.get::<Read<::DB>>().unwrap().get().unwrap();
+    let connection = establish_connection();
     let entries = Entries.limit(5).load::<Entry>(&connection).expect("Error loading entries");
 
     Ok(Response::with((
                 status::Ok,
                 "text/html".parse::<Mime>().unwrap(),
-                "" //layouts::application(views::index::index(entries))
+                layouts::application(views::index::index(entries))
                 )))
 }
 
 pub fn show(request: &mut Request) -> IronResult<Response> {
     let ref id = request.extensions.get::<Router>().unwrap().find("id").unwrap().parse::<i32>().unwrap();
-    let connection = request.extensions.get::<Read<::DB>>().unwrap().get().unwrap();
-    //let entry = Entries.find(id).first::<Entry>(&connection).expect("Error loading entry");
+    let connection = establish_connection();
+    let entry = Entries.find(id).first::<Entry>(&connection).expect("Error loading entry");
     Ok(Response::with((
                 status::Ok,
                 "text/html".parse::<Mime>().unwrap(),
-                "" //layouts::application(views::show::show(entry))
+                layouts::application(views::show::show(entry))
                 )))
 }
